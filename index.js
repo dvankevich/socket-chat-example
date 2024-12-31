4d448525b8b92a2f3453216a72b3234b137d6357
dvankevich <dvankevich@gmail.com>
2024-12-31 19:58:55 +0200

    remove unused code

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  connectionStateRecovery: {},
+  connectionStateRecovery: {
+    maxAttempts: 5, // Максимальна кількість спроб відновлення
+    timeout: 2000, // Час (у мілісекундах) перед наступною спробою
+  },
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -16,40 +19,12 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
-
 io.on("connection", (socket) => {
-  const ip = socket.handshake.address; // Отримання IP-адреси
-  const userAgent = socket.handshake.headers["user-agent"]; // User-Agent
-  const time = new Date(socket.handshake.time);
-  console.log(time.toISOString(), "a user connected:", ip, userAgent);
-
   socket.on("chat message", (msg) => {
-    //console.log(ip, "message: " + msg);
-    io.emit("chat message", ip + " > " + msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected:", ip);
-  });
-
-  // Catch-all listeners
-  socket.onAny((eventName, ...args) => {
-    console.log("onAny:", eventName, " ", args);
-  });
-
-  socket.onAnyOutgoing((eventName, ...args) => {
-    console.log("onAnyOutgoing:", eventName, " ", args);
+    io.emit("chat message", msg);
   });
 });
 
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
 });
-
-console.log("end programm");
